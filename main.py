05b6193fd4ab0639862fee397476065a3ab7f05e
Yatish Goyal <yatishg008@gmail.com>
2026-01-14 15:18:29 +0530

    Some additional features

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import random
 n = random.randint(1, 100)
-a = -1
-guesses = 0
-while(a != n):
-   a = int(input("Guess The No.: "))
-   if(a>n):
+
+def game():
+  a = -1
+  global guesses
+  guesses = 0
+  while(a != n):
+    a = int(input("Guess The No.: "))
+    if(a>n):
       print("Lower No. pls!")
       guesses+=1
-   elif(n>a):
+    elif(n>a):
       print("Higher No pls!")
       guesses+=1
+    elif(n==a):
+     print(f"You Have Guessed The no in {guesses+1} attempt")
+game()
+while True:
+   user = input("Do you want to continue the Game.\nYes/NO: ")
 
-print(f"You Have Guessed The no in {guesses+1} attempt")
+   if user.lower() == "yes":
+      game()
+   else:
+     print("Bye")
+     break
       
